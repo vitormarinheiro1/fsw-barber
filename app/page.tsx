adea144af262ee244a54096739bc1a2a7a39b9f0
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-06 13:06:27 -0300

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -84,6 +84,16 @@ const Home = async () => {
           ))}
         </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2024 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
