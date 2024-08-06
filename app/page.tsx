d36422e87b3644057d4fef597f4e559d2bb7e902
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-06 10:18:56 -0300

    feat: add item de agendamento

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,9 @@ export default function Home() {
           />
         </div>
 
-        <h2 className="text-xs uppercase ">Agendamentos</h2>
+        <h2 className="text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
         <Card className="mt-6">
           <CardContent className="flex justify-between p-0">
             <div className="flex flex-col gap-2 py-5 pl-5">
