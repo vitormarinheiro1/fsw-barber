e29f790eb95a412c50e6757eeef3d08d595f362a
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-07 10:27:07 -0300

    chore: alterando imagem do sidebar

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -15,7 +15,7 @@ const SidebarSheet = () => {
 
       <div className="flex items-center gap-3 border-b border-solid py-5">
         <Avatar>
-          <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <AvatarImage src="https://avatars.githubusercontent.com/u/84048889?v=4" />
         </Avatar>
 
         <div>
