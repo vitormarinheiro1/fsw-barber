70c8957f979ad180603aa97740076edeb4e3e7ea
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-08 17:03:38 -0300

    fix: corrigindo escrita errada na linha 48

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -45,7 +45,7 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPage) => {
       </div>
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para $quot;{searchParams?.title || searchParams?.service}
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
           &quot;
         </h2>
         <div className="grid grid-cols=2 gap-4">
