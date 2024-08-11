11bfb7186a0f14f5bf7773d82e91b726c6b58a87
Vitor <vitormarinheiro9@gmail.com>
2024-08-11 16:55:40 -0300

    feat: somente usuarios autenticados podem agendar

---
+++ app/_lib/auth.ts
@@ -0,0 +1,25 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Adapter } from "next-auth/adapters"
+import { db } from "../_lib/prisma"
+import GoogleProvider from "next-auth/providers/google"
+import { AuthOptions } from "next-auth"
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_URL,
+  callbacks: {
+    async session({ session, user }) {
+      session.user = {
+        ...session.user,
+        id: user.id,
+      } as any
+      return session
+    },
+  },
+}
