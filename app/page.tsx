d2cda2d5d420838e0aee42266494efc41ba921bc
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-08-12 10:26:37 -0300

    feat: page agendamentos

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,26 @@ const Home = async () => {
       name: "desc",
     },
   })
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <div>
@@ -62,7 +82,15 @@ const Home = async () => {
           />
         </div>
 
-        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-2 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
